b215fe4b531affb23590d57753373528e1813107
KirkNotCaptain <cmariekirk@gmail.com>
2021-05-05 12:51:41 -0400

    Installed prettier. Added initial components

---
+++ client/src/components/Beer-Card.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+  name: string;
+  price: number;
+  description: string;
+  active: boolean;
+}
+
+const BeerCard: React.FC<Props> = () => {
+  return <></>;
+};
+
+export default BeerCard;
---
+++ client/src/components/Beer-List.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BeerCard from './Beer-Card';
+
+const BeerList = () => {
+  return (
+    <>
+      <BeerCard />
+    </>
+  );
+};
+
+export default BeerList;
